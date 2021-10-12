1. fixed density calc to the radial proper term. 2. created Numerov algorithm for solving poisson eq 3. started adjusting the hartree potential calculation to radial poisson

hartree_potential.py
@@ -1,22 +1,35 @@
 import numpy as np
 from physical_units import m_e, eps_0, e, hbar
 
-def calculate_hartree_pot(density, xvec, grid_dr, eps=1e-12):
+def calculate_hartree_pot(density, xvec, grid_dr, numelectrons, eps=1e-12):
+    """ U_final = U_poisson +  U_pointwise_potential - (xvec/R) * U_poisson
+        where -  Vhartree = U_final / r
+        (U /r is a trial solution for Vhartree)
+
+    Args:
+        density ([type]): [description]
+        xvec ([type]): [description]
+        grid_dr ([type]): [description]
+        numelectrons ([type]): [description]
+        eps ([type], optional): [description]. Defaults to 1e-12.
+
+    Returns:
+        [type]: [description]
+        [type]: [description]
+    """
+    
     Ha_energy = 0
     Ha_potential = np.zeros(xvec.shape)
-    Ha_energy_constant = - 0.5 *e**2
-    Ha_potential_constant = 4*np.pi 
-    """
-    Ha_potential_constant = - e**2/(4*np.pi * eps_0)
-    for t1 in range(density.size):
-        x1 = xvec[t1]
-        d1 = density[t1]
-        for t2 in range(density.size):
-            x2 = xvec[t2]
-            d2 = density[t2]
-            Ha_energy = Ha_energy + grid_dr**2*(d1*d2)/np.sqrt(np.power(x1-x2,2) + eps)
-            Ha_potential[t1] = Ha_potential[t1] + grid_dr*d2/np.sqrt(np.power(x1-x2,2) + eps)
-   """
+    Ha_energy_constant = - 0.5 
+    Ha_potential_constant = 4*np.pi *e**2
+    # estimation at which r >> R the potential is similar to pointwise charge
+    R = xvec.max() * 10
+    # at r>>R for some large R, the hartree potential is similar to a pointwise charge 
+    U_pointwise_potential = xvec*numelectrons/R 
+
+
+    
+
     for t1 in range(density.size):
         r1 = xvec[t1]
         d1 = density[t1]
@@ -28,7 +41,7 @@ def calculate_hartree_pot(density, xvec, grid_dr, eps=1e-12):
             else:
                 Ha_potential[t1] += (1/r1) * grid_dr * r2 * d2 
         
-        Ha_energy += Ha_energy_constant *Ha_potential_constant *Ha_potential[t1]*d1*grid_dr
+        Ha_energy += Ha_energy_constant *Ha_potential_constant *Ha_potential[t1]*d1*r1**2*grid_dr
 
     Ha_potential = Ha_potential_constant * np.mat(np.diag(Ha_potential, 0))
     return float(Ha_energy), np.asarray(Ha_potential)

ks_solver.py
@@ -58,7 +58,9 @@ class KS_Solver:
             density ([1d matrix(vector)]): [description]
             num_electrons ([integer]): [description]
         """
-        s = float(np.sum(density))
+        #integrate the density over the spherical space
+        #s = float(np.sum(density))
+        s = 4*np.pi * float(np.sum(density * self.grid.gridvec**2 ))
         assert (abs(s - num_electrons) < FLOAT_PRECISION), \
             "density should sum to {0} ! prob={1} instead".format(num_electrons, s)
 
@@ -78,9 +80,8 @@ class KS_Solver:
 
         for i in range (0, len(self.occupation_list)):
             #print("orbital number - {0} adding occupation: {1}".format(i, self.occupation_list[i]))
-            density += self.occupation_list[i] * np.power(np.abs(EigenVecs[:, i]), 2)
-
-
+            #density += self.occupation_list[i] * np.power(np.abs(EigenVecs[:, i]), 2)
+            density += 1/(4*np.pi)* self.occupation_list[i] * np.power(np.abs(EigenVecs[:, i]), 2) /self.grid.gridvec**2
         self._check_density(density, num_electrons)
         return density
 
@@ -148,9 +149,9 @@ class KS_Solver:
 
             x_energy, x_potential = calculate_exchange(density, self.grid.grid_dr)
             Ha_energy, Ha_potential = calculate_hartree_pot(density, self.grid.gridvec,self.grid.grid_dr)
-
+            print("Ha_energy = {0} , x_energy = {1}".format(Ha_energy,x_energy))
             #Veff = Vext + x_potential + Ha_potential
-            Veff = Vext   + x_potential + Ha_potential
+            Veff = Vext + Ha_potential + 0
             # construct the Hamiltonian
             T = get_kinetic_mat(self.grid.gridvec, self.grid.grid_dr)
             H = T + Veff
@@ -159,17 +160,17 @@ class KS_Solver:
             EigenVals, EigenVecs = self._diagonalize_hamiltonian(H)
             
             # ks orbitals visualization
-            for i in range(3):
-                plt.plot(self.grid.gridvec,EigenVecs[:,i],label=EigenVals[i]/e)
-                plt.legend(loc=1)
-            plt.show()
+            # for i in range(3):
+            #     plt.plot(self.grid.gridvec,EigenVecs[:,i],label=EigenVals[i]/e)
+            #     plt.legend(loc=1)
+            # plt.show()
             # calculate new density
             new_density = self._calc_density(EigenVecs, self.numelectrons)
             #print(prev_density)
             print("@@@@@@calculated new density@@@@@\n")
             #print(new_density)
             print(" diff from prev density is:", np.sum(np.abs(prev_density - new_density)))
-            E0 = self.get_ground_state_energy(EigenVals, Ha_energy, x_energy) /e
+            E0 = self.get_ground_state_energy(EigenVals, Ha_energy, 0) /e
             if units == "AU":
                 energy_units = "AU"
             else:
@@ -178,4 +179,4 @@ class KS_Solver:
             iteration += 1
         
         print("finished SCF loop")
-        self.plot_density(self.grid.gridvec, new_density, E0)
+        #self.plot_density(self.grid.gridvec, new_density, E0)

solve_radial_shroedinger.py
@@ -148,8 +148,8 @@ if __name__ == "__main__":
     """ compute probability density for each eigenvector """
     R_functions = Eigen_Vecs
     densities = [np.absolute(R_functions[:, i])**2 for i in range(len(Eigen_Vecs))]
-    analytic_ground_state = get_true_ground_state(Rvec) *Rvec
-    true_density = 4*np.pi *(np.absolute(analytic_ground_state))**2 *np.abs(grid_dr)
+    analytic_ground_state = get_true_ground_state(Rvec)
+    true_density = 4*np.pi *(np.absolute(analytic_ground_state*Rvec))**2 *np.abs(grid_dr)
     densities[2] = true_density
 
     x = np.sum(true_density)

spherical_DFT.py
@@ -16,7 +16,7 @@ print("using {0} units".format(units))
 NumberofElectrons = 2
 
 # DFT params
-maxiterations = 5
+maxiterations = 30
 grid_1d = grid.Radial_Grid(Ngrid, rmin, rmax) 
 initial_density = NumberofElectrons* np.ones(Ngrid)/(Ngrid)
 