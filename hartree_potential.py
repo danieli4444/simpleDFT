fixed initial density and x_energy calculation

hartree_potential.py
@@ -93,7 +93,6 @@ def calculate_hartree_pot(density, xvec, grid_dr, numelectrons, eps=1e-12):
     Ha_energy_constant = 0.5 
     Ha_potential_constant = 4*np.pi/eps_0 *e**2
     # estimation at which r >> R the potential is similar to pointwise charge
-    grid_center = int(len(xvec)/2)
     R = xvec[-3]
     # at r>>R for some large R, the hartree potential is similar to a pointwise charge 
     U_pointwise_potential = xvec*numelectrons/R 
@@ -117,7 +116,7 @@ def calculate_hartree_pot(density, xvec, grid_dr, numelectrons, eps=1e-12):
     # get rid of the numeric artifacts
     for i in range(0,5):
         Vhartree[i] = 0
-        Vhartree[i] = 0
+        Vhartree[-i] = 0
 
     # plt.plot(xvec, Vhartree, color='blue')
     # plt.plot(xvec,density,color='red')
@@ -136,7 +135,7 @@ def calculate_hartree_pot(density, xvec, grid_dr, numelectrons, eps=1e-12):
     print("Ehartree = {0} ".format(Ehartree))
     print("Ehartree with scipy.simps integration = {0}".format(Ehartree2))
 
-    return float(Ehartree) , np.asarray(np.mat(np.diag(Vhartree, 0)))
+    return float(Ehartree2) , np.asarray(np.mat(np.diag(Vhartree, 0)))
 
 
 

ks_solver.py
@@ -153,8 +153,13 @@ class KS_Solver:
 
             x_energy, x_potential = calculate_exchange(density, self.grid.gridvec, self.grid.grid_dr)
             Ha_energy, Ha_potential = calculate_hartree_pot(density, self.grid.gridvec,self.grid.grid_dr,self.numelectrons)
+            plt.figure(1)
+            plt.plot(self.grid.gridvec,density,color='red')
+            plt.figure(2)
+            plt.plot(self.grid.gridvec,np.diag(Ha_potential),color='blue')
+            plt.show()
             #Veff = Vext + x_potential + Ha_potential
-            Veff = Vext + Ha_potential + x_potential
+            Veff = Vext + Ha_potential + 0
             # construct the Hamiltonian
             T = get_kinetic_mat(self.grid.gridvec, self.grid.grid_dr)
             H = T + Veff
@@ -163,10 +168,10 @@ class KS_Solver:
             EigenVals, EigenVecs = self._diagonalize_hamiltonian(H)
             
             # ks orbitals visualization
-            # for i in range(3):
-            #     plt.plot(self.grid.gridvec,EigenVecs[:,i],label=EigenVals[i]/e)
-            #     plt.legend(loc=1)
-            # plt.show()
+            for i in range(3):
+                plt.plot(self.grid.gridvec,EigenVecs[:,i],label=EigenVals[i]/e)
+                plt.legend(loc=1)
+            plt.show()
             # calculate new density
             new_density = self._calc_density(EigenVecs, self.numelectrons)
             #print(prev_density)

spherical_DFT.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy import integrate
 from ks_solver import KS_Solver
 import grid
 from physical_units import units,rmin,rmax
@@ -18,8 +18,10 @@ NumberofElectrons = 1
 # DFT params
 maxiterations = 30
 grid_1d = grid.Radial_Grid(Ngrid, rmin, rmax) 
-initial_density = NumberofElectrons* np.ones(Ngrid)/(Ngrid)
-
+#initial_density = np.exp(-2*grid_1d.gridvec)/np.pi
+initial_density = NumberofElectrons**4 /(64*np.pi) * np.exp(-NumberofElectrons*grid_1d.gridvec/2)
+#check_density = 4*np.pi *integrate.simps(initial_density*grid_1d.gridvec**2,grid_1d.gridvec)
+#print(check_density)
 ks_1d = KS_Solver(grid_1d, NumberofElectrons, maxiterations, initial_density,convergence_threshold=0.001)
 
 ks_1d.runSCF()