fixed normalization issues on eigenvector evaluation

grid.py
@@ -8,18 +8,7 @@ class Radial_Grid:
         self.rmin = rmin
         self.rmax = rmax
         self.r_gridsize = r_gridsize
-        (self.gridvec, self.grid_dr) = np.linspace(self.rmin, self.rmax, self.r_gridsize \
+        (self.gridvec, self.grid_dr) = np.linspace(self.rmax, self.rmin, self.r_gridsize \
                 ,endpoint=False,retstep=True)
         self.gridvec = self.gridvec[::-1]
         
-
-class  Cartesian_1d:
-    def __init__(self, x_gridsize , xmin ,xmax) -> None:
-        self.grid_type = GRID_TYPES[1]
-        self.xmin = xmin
-        self.xmax = xmax
-        self.x_gridsize = x_gridsize
-        (self.gridvec, self.grid_dr) = np.linspace(self.xmin, self.xmax, self.x_gridsize \
-                ,endpoint=False,retstep=True)
-
-       

hartree_potential.py
@@ -1,6 +1,6 @@
 import numpy as np
-import scipy
 from scipy.integrate.odepack import odeint
+from scipy.optimize.zeros import _compute_divided_differences
 from physical_units import m_e, eps_0, e, hbar
 from scipy import integrate
 import matplotlib.pyplot as plt
@@ -101,38 +101,27 @@ def calculate_hartree_pot(density, xvec, grid_dr, numelectrons, eps=1e-12):
 
     # U_poisson'' = - 4pi * r * density(r)
     U_poisson = solve_poisson_ode(xvec,density)
-    #plt.plot(xvec,U_poisson,color='red')
-    #plt.show()
+
     #print(U_poisson[-100]/xvec[-100])
     #print(U_pointwise_potential[-100]/xvec[-100])
     # normalization??
-    #norm = integrate.simps(U_poisson**2,x=xvec)
-    #U_poisson *= 1/np.sqrt(abs(norm))
+    # norm = 4*np.pi *integrate.simps(xvec**2 * U_poisson**2,x=xvec)
+    # U_poisson *= 1/np.sqrt(abs(norm))
 
     U_final = U_poisson +  U_pointwise_potential - (xvec/R) * U_poisson
 
-    print("U_poisson = {0} , U_pointwise = {1}".format(U_poisson.max(), U_pointwise_potential.max()))
     Vhartree = U_final/xvec
+    Ehartree2 = Ha_energy_constant * 4*np.pi * integrate.simps(Vhartree* density * xvec**2 ,xvec)
+    # for t in range(len(Vhartree)):
+    #     r = xvec[t]
+    #     d = density[t] 
+    #     Ehartree = 0
+    #     Ehartree += Vhartree[t] * d * r**2 * grid_dr
+    # Ehartree *= Ha_energy_constant 
+    # Ehartree = Ha_energy_constant * 4*np.pi (Vhartree*density*xvec**2,xvec)
+    # print("Ehartree = {0} ".format(Ehartree))
 
-    # get rid of the numeric artifacts
-    for i in range(0,5):
-        Vhartree[i] = 0
-        Vhartree[-i] = 0
-
-    # plt.plot(xvec, Vhartree, color='blue')
-    # plt.plot(xvec,density,color='red')
-
-    for t in range(len(Vhartree)):
-        r = xvec[t]
-        d = density[t] 
-        Ehartree = 0
-        Ehartree += Vhartree[t] * d * r**2 * grid_dr
-    
-    Ehartree *= Ha_energy_constant 
-    Ehartree2 = integrate.simps(Vhartree* density * xvec**2 ,xvec)
-    #Ehartree = Ha_energy_constant * 4*np.pi (Vhartree*density*xvec**2,xvec)
     print("solved Vhartree using poisson!")
-    print("Ehartree = {0} ".format(Ehartree))
     print("Ehartree with scipy.simps integration = {0}".format(Ehartree2))
 
     return float(Ehartree2) , np.asarray(np.mat(np.diag(Vhartree, 0)))
@@ -147,8 +136,9 @@ if __name__ == "__main__":
 
     Ngrid = 2000
     rmax = 40.0
-    rmin = 1e-10
-    (xvec, grid_dr) = np.linspace(rmin, rmax, Ngrid,retstep=True)
+    rmin = 1e-5
+    (xvec, grid_dr) = np.linspace(rmax, rmin, Ngrid,endpoint=False,retstep=True)
+    xvec = xvec[::-1]
     numelectrons=1
     R=rmax
 
@@ -160,16 +150,23 @@ if __name__ == "__main__":
     # computed_density = check_potential(U_potential,xvec,grid_dr)
     # plt.plot(xvec[3:-5],computed_density[3:-5],color='red',label="solved d2U/d2r")
         
-    density_1s = np.exp(-2*xvec)/np.pi
+    density_1s =  np.exp(-xvec)/(8*np.pi)
     U_potential_1s = solve_poisson_ode(xvec,density_1s)
+    
     U_pointwise_potential = xvec*numelectrons/rmax
     U_final = U_potential_1s +  U_pointwise_potential - (xvec/rmax) * U_potential_1s
     V_potential_1s = U_final/xvec
-    xvec=xvec
     density_1s = density_1s
-    plt.plot(xvec,U_potential_1s,color='blue')
-    plt.show()
+    plt.plot(xvec,density_1s,color='blue')
     E_1s = 4*np.pi * integrate.simps(xvec**2 * density_1s * V_potential_1s,xvec)
+    density_1s_integral = 4*np.pi * integrate.simps(xvec**2 *density_1s ,xvec)
+    print("the density sums to :",density_1s_integral)
+    x = -4*np.pi *xvec*density_1s
+    computed_density = check_potential(U_final,xvec,grid_dr)
+    # plt.plot(xvec[1:-2],x[1:-2],color='blue',label="ground truth-4pi*x *p(x)")
+    # plt.plot(xvec[1:-2],computed_density[1:-2],color='red',label="solved d2U/d2r")
+
+    plt.show()
     print("the Hartree energy for 1s Hydrodgen is (should be 0.625):",E_1s)
     
 

ks_solver.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.core.function_base import linspace
 from numpy.linalg import eigvals
-from scipy import constants as const
+from scipy import constants as const, integrate
 
 
 from hartree_potential import calculate_hartree_pot
@@ -43,14 +43,7 @@ class KS_Solver:
             occupation_list.append(1)
         return occupation_list
 
-    def _check_normalized_state(self, state):
-        s = 0
-        for k in state:
-            s = s + k**2
-        s = float(s)
-        assert (abs(s - 1) <
-                FLOAT_PRECISION), "probability of state should sum to 1! prob={0} instead".format(s)
-    
+
     def _check_density(self,density, num_electrons):
         """ verifies that the density sums to number of electrons
 
@@ -58,11 +51,15 @@ class KS_Solver:
             density ([1d matrix(vector)]): [description]
             num_electrons ([integer]): [description]
         """
+
+        FLOAT_PRECISION = 0.01
         #integrate the density over the spherical space
         #s = float(np.sum(density))
-        s = 4*np.pi * float(np.sum(density * self.grid.gridvec**2 ))
+        #s = 4*np.pi * float(np.sum(density * self.grid.gridvec**2 ))
+        s = 4*np.pi * integrate.simps(density * self.grid.gridvec**2 ,self.grid.gridvec)
+        print("the density sums to ",s)
         assert (abs(s - num_electrons) < FLOAT_PRECISION), \
-            "density should sum to {0} ! prob={1} instead".format(num_electrons, s)
+            "density should sum to {0} ! got prob={1} instead".format(num_electrons, s)
 
     
     #ToDo: change occupation type to spin orbitals
@@ -74,14 +71,13 @@ class KS_Solver:
             EigenVecs ([type]): [description]
             num_electrons ([type]): [description]
         """    
-        test_state = EigenVecs[:, 2]
-        self._check_normalized_state(test_state)
         density = 0
 
         for i in range (0, len(self.occupation_list)):
             #print("orbital number - {0} adding occupation: {1}".format(i, self.occupation_list[i]))
             #density += self.occupation_list[i] * np.power(np.abs(EigenVecs[:, i]), 2)
-            density += 1/(4*np.pi)* self.occupation_list[i] * np.abs(EigenVecs[:, i])**2 /self.grid.gridvec**2
+            density += self.occupation_list[i] * np.abs(EigenVecs[:, i])**2 
+
         self._check_density(density, num_electrons)
         return density
 
@@ -108,11 +104,15 @@ class KS_Solver:
         plt.show()
         return
 
-    def get_ground_state_energy(self, EigenVals, Ehartree, x_energy):
+    def get_ground_state_energy(self, EigenVals, Ehartree, x_energy,newdensity,x_potential):
         E0 = 0
         E_ks = 0
-        E_integrated_xc = 0
         E_xc = x_energy
+
+
+        E_integrated_xc = 4*np.pi * integrate.simps(newdensity*np.diag(x_potential)*self.grid.gridvec**2,self.grid.gridvec)
+
+
 
         for i in range (0, len(self.occupation_list)):
             E_ks += EigenVals[i] * self.occupation_list[i]
@@ -133,6 +133,7 @@ class KS_Solver:
         """
         prev_density = np.zeros(self.grid.gridvec.shape) # just to initiate the scf loop
         iteration = 0
+        self._check_density(self.initial_density,self.numelectrons)
         new_density = self.initial_density
         #print(np.sum(np.abs(prev_density - new_density)))
         print(" grid type - ", self.grid_type)
@@ -150,32 +151,38 @@ class KS_Solver:
 
             x_energy, x_potential = calculate_exchange(density, self.grid.gridvec, self.grid.grid_dr)
             Ha_energy, Ha_potential = calculate_hartree_pot(density, self.grid.gridvec,self.grid.grid_dr,self.numelectrons)
-            plt.figure(1)
-            plt.plot(self.grid.gridvec,density,color='red')
-            plt.figure(2)
-            plt.plot(self.grid.gridvec,np.diag(Ha_potential),color='blue')
-            plt.show()
+ 
             #Veff = Vext + x_potential + Ha_potential
-            Veff = Vext + 0 + 0
+            Veff = Vext + Ha_potential + 0
             # construct the Hamiltonian
             T = get_kinetic_mat(self.grid.gridvec, self.grid.grid_dr)
-            H = T + Veff
-            
+            H = - T + Veff
             # solve Kohn-Sham equations - find eigenvalues and eigenvectors for the hamiltonian
             EigenVals, EigenVecs = self._diagonalize_hamiltonian(H)
+
+            # Eigen Vectors normalization (spherical density)
+            for i in range(len(EigenVals)):
+                norm_factor = 4*np.pi * integrate.simps(EigenVecs[:,i]**2 *self.grid.gridvec**2,self.grid.gridvec)
+                EigenVecs[:,i] /= np.sqrt(norm_factor)
+  
+
             
             # ks orbitals visualization
-            for i in range(3):
-                plt.plot(self.grid.gridvec,EigenVecs[:,i],label=EigenVals[i]/e)
-                plt.legend(loc=1)
-            plt.show()
+            # for i in range(3):
+            #     plt.plot(self.grid.gridvec,EigenVecs[:,i],label=EigenVals[i]/e)
+            #     plt.legend(loc=1)
+            # plt.show()
             # calculate new density
-            new_density = self._calc_density(EigenVecs, self.numelectrons)
+            new_density = (1/4*np.pi)*self._calc_density(EigenVecs, self.numelectrons)
             #print(prev_density)
             print("@@@@@@calculated new density@@@@@\n")
+            plt.figure(2)
+            plt.plot(self.grid.gridvec,density,color='red')
+            plt.plot(self.grid.gridvec,new_density,color='blue')
+            plt.show()
             #print(new_density)
             print(" diff from prev density is:", np.sum(np.abs(prev_density - new_density)))
-            E0 = self.get_ground_state_energy(EigenVals, Ha_energy, x_energy) /e
+            E0 = self.get_ground_state_energy(EigenVals, Ha_energy, x_energy,new_density,x_potential) /e
             if units == "AU":
                 energy_units = "AU"
             else:

solve_radial_shroedinger.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from numpy.core.function_base import linspace
 from numpy.linalg import eigvals
 from scipy import constants as const
+from scipy import integrate
 
 # phisical constants
 omega = 2*np.pi
@@ -27,7 +28,6 @@ eps_0 = 1/ (4* np.pi)
 
 
 a0 = (4*np.pi * eps_0 * hbar**2)/(m_e * e**2)
-print("a0 = {0}".format(a0))
 
 
 """
@@ -129,15 +129,14 @@ def get_true_ground_state(Rvec):
 
 
 # grid constants:
-R_gridsize = 1000
+R_gridsize = 2000
 Rmin = 1e-05
-Rmax = 20
+Rmax = 40
 
 
 if __name__ == "__main__":
-    (Rvec, grid_dr) = np.linspace(Rmax,Rmin , R_gridsize,endpoint=False,retstep=True)
+    (Rvec, grid_dr) = np.linspace(Rmax, Rmin, R_gridsize,endpoint=False,retstep=True)
     Rvec = Rvec[::-1]
-    print(Rvec)
     #(phy_vec, d_phy) = np.linspace(phy_min, phy_max, phy_gridsize,retstep=True)
     #(theta_vec, d_theta)= np.linspace(theta_min, theta_max, theta_gridsize,retstep=True)
     #theta_vec, phy_vec = np.meshgrid(theta_vec, phy_vec)
@@ -151,16 +150,26 @@ if __name__ == "__main__":
 
     H = -kinetic_term + angular_term - potential_term 
     (Eigen_Vals,Eigen_Vecs) = diagonalize_hamiltonian(H)
+    for i in range(len(Eigen_Vals)):
+                norm_factor = 4*np.pi *integrate.simps(Eigen_Vecs[:,i]**2 *Rvec**2,Rvec)
+                Eigen_Vecs[:,i] /= np.sqrt(norm_factor)
 
     """ compute probability density for each eigenvector """
     R_functions = Eigen_Vecs
-    densities = [np.absolute(R_functions[:, i])**2 for i in range(len(Eigen_Vecs))]
+    densities = [np.absolute(R_functions[:, i])**2 for i in range(len(Eigen_Vals))]
     analytic_ground_state = get_true_ground_state(Rvec)
     true_density = 4*np.pi *(np.absolute(analytic_ground_state*Rvec))**2 *np.abs(grid_dr)
     densities[2] = true_density
 
-    x = np.sum(true_density)
-    print("density sums to ",np.sum(x))
+    from scipy import integrate
+    dens = analytic_ground_state**2
+    #densities[2] = dens
+
+    x = 4*np.pi * integrate.simps(densities[0]*Rvec**2,Rvec)
+    print("the density sums to ",x)
+    y = 4*np.pi * integrate.simps(dens*Rvec**2,Rvec)
+    print("the density sums to ",y)
+
     plot_densities(Rvec, densities, Eigen_Vals)
     #plt.plot(Rvec*1e10,Eigen_Vecs[0]**2)
     #plt.show()

spherical_DFT.py
@@ -6,20 +6,21 @@ import grid
 from physical_units import units,rmin,rmax
 
 # numeric paramters
-FLOAT_PRECISION = 1e-05
-Ngrid = 2000
+FLOAT_PRECISION = 1e-02
+Ngrid = 3000
 
 # remember set units choice in the physical_units.py file
 print("using {0} units".format(units))
 
 # molecule params:
 NumberofElectrons = 1
-
 # DFT params
 maxiterations = 30
 grid_1d = grid.Radial_Grid(Ngrid, rmin, rmax) 
 #initial_density = np.exp(-2*grid_1d.gridvec)/np.pi
 initial_density = NumberofElectrons**4 /(64*np.pi) * np.exp(-NumberofElectrons*grid_1d.gridvec/2)
+#initial_density =   np.exp(-2*grid_1d.gridvec)/np.pi
+
 #check_density = 4*np.pi *integrate.simps(initial_density*grid_1d.gridvec**2,grid_1d.gridvec)
 #print(check_density)
 ks_1d = KS_Solver(grid_1d, NumberofElectrons, maxiterations, initial_density,convergence_threshold=0.001)

xc_potential.py
@@ -4,7 +4,7 @@ from scipy import integrate
 
 #ToDo: check if dX is needed in the energy calc here??
 def calculate_exchange(density,xvec,grid_dr):
-    x_energy_const = -(3/4)*np.power((3/np.pi),1/3)
+    x_energy_const = (3/4)*np.power((3/np.pi),1/3)
     x_potential_const = -np.power((3/np.pi),1/3)
    
     x_energy = x_energy_const * 4*np.pi * integrate.simps(np.power(density,4/3)*xvec**2,xvec)